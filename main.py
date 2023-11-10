7c1a03a8dff7f36e79a0099e2d12ae6522bdbe19
Copeland Carter <smartycope@gmail.com>
2023-11-09 18:21:02 -0700

    removed key, updated local

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from code_editor import code_editor
 
-resp = code_editor('st.markdown("hello world")', key='this')
-_local = {}
+resp = code_editor('st.markdown("Hello world!")')
+_local = locals()
 _global = globals()
 exec(resp['text'], _global, _local)
 globals().update(_global)
